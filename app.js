3f62705c54c86f1ece9a01f6a4f6977b6d2d587e
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-02-22 00:37:14 -0500

    fixed an issue where notes were being written twice

--- app.js
+++ app.js
@@ -9,12 +9,7 @@ let db = require('./db/db.json');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './public')));
-// app.use(express.static('./'));
 
-
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
   
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
@@ -24,29 +19,28 @@ app.get("/api/notes", function(req, res) {
     res.json(db)
  });
 
+ app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
  
 
-  app.post("/api/notes", function(req, res) {
+app.post("/api/notes", function(req, res) {
     const newNote = req.body;
+    console.log(newNote)
     db.push(newNote)
   
-    console.log(db);
-  
-    fs.writeFile(path.join(__dirname, "'./db/db.json'"), 
+    fs.writeFile(path.join(__dirname, "db/db.json"), 
     JSON.stringify(db), 
     err => {
       if(err) {
         res.json(err)
       } else {
-        res.json(data)
+        res.json(db)
       }
     })
-  
-    res.json(newCharacter);
   });
   
-
-
   
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
