b94f2bc12475bab609da1c40e717c99aa88658fb
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-02-22 23:40:51 -0500

    Still work in progress

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 let db = require('./db/db.json');
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-  
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
@@ -28,6 +26,7 @@ app.post("/api/notes", (req, res) => {
     const note = req.body;
     note.id = Math.floor(Math.random() * 9999)
     db.push(note)
+  
     fs.writeFile(path.join(__dirname, "db/db.json"), 
     JSON.stringify(db), 
     err => {
@@ -39,7 +38,26 @@ app.post("/api/notes", (req, res) => {
   })
 });
 
-
+app.delete("/api/notes/:id", (req, res) => {
+  console.log(req.params.id)
+  console.log(db.length)
+  let grabId = req.params.id
+  for(let i = 0; i < db.length; i++){
+    if(grabId == db[i].id){
+      db.splice(i, 1);
+      break
+    }
+  }
+  fs.writeFile(path.join(__dirname, "db/db.json"), 
+  JSON.stringify(db), 
+  err => {
+    if(err) {
+      res.json(err)
+    } else {
+      res.json(db)
+    }
+  })
+})
 
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
